ea3d2de635701d81aab4e8f661d95c5141e445f7
Andrew Chambers <ac@acha.ninja>
2020-08-26 19:44:57 +1200

    Work on first release.

---
+++ support/src-release.sh
@@ -0,0 +1,23 @@
+set -eux
+
+version="$1"
+
+rm -rf ./release/
+mkdir ./release
+mkdir release/src
+mkdir release/src/.cargo
+git archive $version | tar -C release/src -x -f -
+cd release/src
+
+cargo vendor > .cargo/config
+
+tar -cvf - . | gzip -9 > ../../bupstash-src.tar.gz
+
+cd ..
+mkdir man
+cd man
+cp ../src/doc/man/*.md ./
+ronn -r *.md
+rm *.md
+
+tar -cvf - . | gzip -9 > ../../bupstash-man.tar.gz
